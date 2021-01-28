d5f6feed6e6bf283a6048c5d94a8e0f1135003c8
Christine Lyston <calyston.14@gmail.com>
2021-01-28 17:55:48 +0000

    style: grid items aligned

--- about.js
+++ about.js
@@ -1,31 +1,25 @@
 //Strapi About Page Call
 
-const aboutText = document.getElementById('aboutText')
+const mainDiv = document.getElementById('aboutInfo')
 
-const aboutImage = document.getElementById('aboutImage')
-
- 
 fetch('http://167.99.193.212/about-page')
-  .then(res=>res.json())
-  .then((data)=>{
+  .then(res => res.json())
+  .then((data) => {
     createHtml(data)
     return
   })
-  .catch(err=>{
+  .catch(err => {
     console.log(err)
   })
- 
-const createHtml = (pageInfo) =>{
-  const {aboutImage, aboutText} = pageInfo
 
-  const pageText = (`
+const createHtml = (pageInfo) => {
+  const { aboutImage, aboutText } = pageInfo
+  const pageHtml = (`
+    <div>
       <p>${aboutText}</p>
+      <img class="about-img" src=http://167.99.193.212${aboutImage.url}>
+    </div>
   `)
-  const pageImage = (`
-    <img class="about-img" src=http://167.99.193.212${aboutImage.url}>
-  `)
-
-  aboutText.innerHTML = pageHtml
-  aboutImage.innerHTML = pageHtml
+  mainDiv.innerHTML = pageHtml
   return
 }
